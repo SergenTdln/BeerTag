88a11ad9a5d1d102bdc239f33e7b6c1e4eba53e2
Martin Vandenbussche <martin.vandenbussche@student.uclouvain.be>
2019-02-23 12:46:54 +0100

    Réflexions pour la structure des classes d'entités

--- app/src/main/java/application_projet4_groupe12/entities/Business.java
+++ app/src/main/java/application_projet4_groupe12/entities/Business.java
@@ -4,4 +4,7 @@ package application_projet4_groupe12.entities;
  * Entities of this class can represent any shop, bar or any establishment in which users can earn points.
  */
 public class Business {
+
+    private String name;
+    private String imagePath; //TODO : Should we store all files in the assets folder or "stream" them from the database ? (In the later case, this variable could directly store the blob object)
 }
--- app/src/main/java/application_projet4_groupe12/entities/UserAccount.java
+++ app/src/main/java/application_projet4_groupe12/entities/UserAccount.java
@@ -1,4 +1,12 @@
 package application_projet4_groupe12.entities;
 
+/**
+ * Entities of this class represent users of the app.
+ */
 public class UserAccount {
+
+    private String loginEmail;
+    private String firstName;
+    private String lastName;
+    private int age; //TODO: We might want to restrict accesss to certain products/offers for minors ?
 }

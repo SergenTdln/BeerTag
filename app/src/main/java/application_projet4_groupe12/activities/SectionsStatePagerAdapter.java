73749239a7d6a86c0aebcee0ea0c98076502c90b
kerry <raczkerry@gmail.com>
2019-03-04 14:20:49 +0100

    Classe SignUp ajoutée SectionStatePageAdapter ajouté Fragment1 et 2 ajouté
    La base de données a été modifiée, user ajouté manuelement avec SQLite Studio

---
+++ app/src/main/java/application_projet4_groupe12/activities/SectionsStatePagerAdapter.java
@@ -0,0 +1,38 @@
+package application_projet4_groupe12.activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public void addFragment(Fragment fragment, String title){
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    public SectionsStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public float getPageWidth(int position) {
+        return 0.9f;
+    }
+}
---
+++ app/src/main/java/application_projet4_groupe12/activities/SignUp.java
@@ -0,0 +1,36 @@
+package application_projet4_groupe12.activities;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.application_projet4_groupe12.R;
+
+public class SignUp extends AppCompatActivity {
+
+    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+
+        setupViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment1(), "Fragment1");
+        adapter.addFragment(new Fragment2(), "Fragment2");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber) {
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+}

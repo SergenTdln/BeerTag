9fd3d4f2c237befa092bed42f663d17eee40cf6b
AlexandreKara <alexkara.perso@outlook.com>
2019-03-05 16:59:10 +0100

    Je commit ce que j'ai, Transfer() est une première façon de transférer tous les documents dans sqlite et Transfer2() en est une autre façon. A noter qu'aucune des deux ne fonctionne, je ne suis pas encore parvenu à trouver pourquoi concernant Transfer(). Par contre concernant Transfer2(), je ne sais pas si ça la fera fonctionner mais, je ne parviens pas à get l'id car il est sous forme de int ( un number ) et pas sous forme de string. Et finalement pour ce qui est d'ajouter un document dans la base de données firebase c'est dab.collection("Users").add(user); . Users étant la collection et user le document en question qu'on ajoute à cette collection. Et pour modifier/remplacer un document c'est similaire mais avec set() au-lieu de add().

--- app/src/main/java/application_projet4_groupe12/activities/Fragment2.java
+++ app/src/main/java/application_projet4_groupe12/activities/Fragment2.java
@@ -1,14 +1,32 @@
 package application_projet4_groupe12.activities;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.protobuf.Any;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import application_projet4_groupe12.R;
 
@@ -23,11 +41,15 @@ import application_projet4_groupe12.entities.User;
 import application_projet4_groupe12.exceptions.WrongDateFormatException;
 import application_projet4_groupe12.exceptions.WrongEmailFormatException;
 
+import static android.content.ContentValues.TAG;
+import static java.lang.System.err;
+
 public class Fragment2 extends Fragment {
 
     private Button fragment2_sign_up;
     private SQLHelper db;
     private User user;
+    private FirebaseFirestore dab = FirebaseFirestore.getInstance();
 
     @Nullable
     @Override
@@ -46,23 +68,21 @@ public class Fragment2 extends Fragment {
                     EditText confirmPassword = (EditText)getView().findViewById(R.id.editText5);
 
                     if (db.doesUsernameExist(username.getText().toString()))  {
-                        Toast.makeText(getActivity(),  R.string.existing_email, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(),  "This email already exists", Toast.LENGTH_SHORT).show();
                     }
                     else {
                         if (password.getText().toString().equals(confirmPassword.getText().toString())) {
-
                             int id = db.getFreeIDUser();
                             Date date = Calendar.getInstance().getTime();
                             DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
                             String today = formatter.format(date);
-
                             user = new User(id, id, username.getText().toString(), today,  "albert", "le chat");
-
                             db.createUser(user);
-                            Toast.makeText(getActivity(), R.string.account_done, Toast.LENGTH_SHORT).show();
+                            dab.collection("Users").add(user);
+                            Toast.makeText(getActivity(), "Account created", Toast.LENGTH_SHORT).show();
                         }
                         else {
-                            Toast.makeText(getActivity(), R.string.password_no_match, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getActivity(), "Passwords do not match", Toast.LENGTH_SHORT).show();
                         }
                     }
 
@@ -79,5 +99,60 @@ public class Fragment2 extends Fragment {
         });
 
         return view;
+    }
+    public void Transfer(){
+        dab.collection("Users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                user = document.toObject(User.class);
+                                try {
+                                    db.createUser(user);
+                                }
+                                catch (WrongEmailFormatException e) {
+                                    e.printStackTrace();
+                                }
+                                catch (WrongDateFormatException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+    public void Transfer2(){
+        dab.collection("Users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                User user = document.toObject(User.class);
+                                int id = Integer.parseInt(document.getString("id"));
+                                String today = document.getString("creationDate");
+                                String firstname = document.getString("firstName");
+                                String lastname = document.getString("lastName");
+                                user = new User(id, id, document.getString("username"), today,  firstname, lastname);
+                                try {
+                                    db.createUser(user);
+                                }
+                                catch (WrongEmailFormatException e) {
+                                    e.printStackTrace();
+                                }
+                                catch (WrongDateFormatException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }

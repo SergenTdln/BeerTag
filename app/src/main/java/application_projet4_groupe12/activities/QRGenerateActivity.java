69c0b39d0faa2a65e497514587536ce337ecef4a
SergenTdln <fb.sergen.tasdelen@gmail.com>
2019-04-24 16:16:06 +0200

    Refonte design scan & generate plus couleur toolbar

--- app/src/main/java/application_projet4_groupe12/activities/QRGenerateActivity.java
+++ app/src/main/java/application_projet4_groupe12/activities/QRGenerateActivity.java
@@ -1,8 +1,6 @@
 package application_projet4_groupe12.activities;
 
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
